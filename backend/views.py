4c5921889b224ed5409c1d4ab8eadb88a8d05f83
Antonio Cesarano <cesarano2607@gmail.com>
2018-10-28 01:54:43 +0200

    docker: add link between backend and scraper_site

--- backend/views.py
+++ backend/views.py
@@ -14,3 +14,22 @@ def home():
 def api():
     return jsonify(success=True, status_code=200)
 
+
+@bp_api.route('/test', methods=['GET'])
+def test():
+    import os
+    import requests
+
+    try:
+        scraper_url = "http://{}:{}".format(
+            os.environ['SCRAPER_SITE_1_PORT_5000_TCP_ADDR'],
+            os.environ['SCRAPER_SITE_PORT_5000_TCP_PORT'],
+        )
+    except KeyError:
+        scraper_url = "http://0.0.0.0:8080"
+
+    resp = requests.get(scraper_url).text
+    envs = {k: v for (k, v) in os.environ.items()}
+
+    resp = {'scraper': resp, 'env': envs}
+    return jsonify(resp)
